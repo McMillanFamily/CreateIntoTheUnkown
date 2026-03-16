0e96ffe9fb3a8deeac6f511c7e04d7a6376c439d
McMillanfamily <lewis.mcmillan09@gmail.com>
2026-03-16 17:43:22 +0000

    Initial commit

---
+++ src/main/java/net/mcmillanfamily/citu/CreateIntoTheUnknownDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mcmillanfamily.citu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateIntoTheUnknownDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
